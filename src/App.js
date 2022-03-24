a210890a657b23e80c4ebfba0d2bd646162deca8
Ian Applebaum <Ian.tyler@electricbluefish.com>
2022-03-24 11:54:32 -0400

    Added axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import React, {useEffect} from 'react';
+import {axios} from 'axios';
 
 function App() {
   const [duck, setDuck] = React.useState("https://random-d.uk/api/87.jpg");
-
+  const getDuck = async () => {
+    let json
+    await axios.get("http://random-d.uk/api/v2/quack")
+        .then(function (response){
+          json = response.data
+          // console.log(json)
+        })
+        .catch(err => {
+          alert("something went wrong:"+err);
+          //location.reload();
+        })
+  }
   return (
     <div className="App">
       <header className="App-header">
